c2f8d31229e92b9158a3b896a5d38eea2f4e1a57
choisungwook <kgg1959@naver.com>
2021-07-19 08:05:21 +0900

    Alluser router추가

---
+++ src/views/Users.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+      <v-row>
+          <v-col>
+              <AllUsers />
+          </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+// import AllUsers from '@/components/Users/AllUsers'
+import AllUsers from '../components/Users/Allusers'
+
+export default {
+    components: {
+        AllUsers,
+    },
+}
+</script>
+
+<style>
+
+</style>
