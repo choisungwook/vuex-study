a049b40dbd8f501d42b9ed3c981431a0fbc91a14
choisungwook <kgg1959@naver.com>
2021-07-19 07:15:19 +0900

    데이터 추가

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,11 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    allUsers: [
+      {userId: 'aaa', password: '123', name: 'aaa', address: 'Seoul', src: 'http://aaa.com'},
+      {userId: 'bbb', password: '123', name: 'bbb', address: 'Seoul', src: 'http://bbb.com'},
+      {userId: 'bbb', password: '123', name: 'bbb', address: 'Seoul', src: 'http://ccc.com'},
+    ]
   },
   mutations: {
   },
