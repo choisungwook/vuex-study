e1a0f98e6d11a7dfe2e4420149762eff29103d4f
choisungwook <kgg1959@naver.com>
2021-07-19 10:02:23 +0900

    fix: vuetify tag 수정
    존재하지 않는 tag가 있어 유효한 tag로 수정

--- src/components/Users/Allusers.vue
+++ src/components/Users/Allusers.vue
@@ -5,7 +5,7 @@
         <v-list-item v-for="(user, index) in $store.state.allUsers" :key="user.index">
             <v-list-item-content>
               <v-list-item-title v-text="user.name"></v-list-item-title>
-              <v-list-tile-sub-title>id:#{{index}} / {{user.address}} 거주</v-list-tile-sub-title>
+              <v-list-item-title>id:#{{index}} / {{user.address}} 거주</v-list-item-title>
             </v-list-item-content>
         </v-list-item>
       </v-list>
