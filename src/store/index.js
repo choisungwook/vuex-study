84de7fd4370c0048f0e7072f56e643799bdfcb91
choisungwook <kgg1959@naver.com>
2021-07-19 10:33:28 +0900

    디폴트 유저목록 변경

--- src/store/index.js
+++ src/store/index.js
@@ -6,9 +6,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     allUsers: [
-      {userId: 'aaa', password: '123', name: 'aaa', address: 'Seoul', src: 'http://aaa.com'},
-      {userId: 'bbb', password: '123', name: 'bbb', address: 'Seoul', src: 'http://bbb.com'},
-      {userId: 'bbb', password: '123', name: 'bbb', address: 'Seoul', src: 'http://ccc.com'},
+      {userId: 'defaultUser', password: 'password', name: 'defaultUser', address: 'Seoul', src: 'http://aaa.com'},
     ]
   },
   mutations: {
